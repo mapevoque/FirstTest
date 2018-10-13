d5c311e0696a21b9a7887aa7f52b3c09f6ee91ae
mapevoque <40743421+mapevoque@users.noreply.github.com>
2018-10-13 23:31:30 +0300

    Delete conf3.js

--- conf3.js
+++
@@ -1,9 +0,0 @@
-exports.config = {
-  seleniumAddress: 'http://localhost:4444/wd/hub',
-  suites: {
-    search: ['tests/e2e/contact_search/**/*as.js']
-	},
-	  jasmineNodeOpts: {
-    showColors: true, 
-  }
-  };
